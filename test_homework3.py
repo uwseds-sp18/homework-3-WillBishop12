bde4f48ecd9ab9bf4df086edf65b94c538fd1215
Will Bishop <william.d.bishop.12@dartmouth.edu>
2018-04-14 14:33:17 -0700

    HW3 correction

--- test_homework3.py
+++ test_homework3.py
@@ -1,15 +1,30 @@
-import sqlite3
-import pandas as pd
+import unittest
+import homework3
 
-#According to https://tutorials.technology/tutorials/08-How-to-check-that-file-exists-with-Python.html, importing Path function from pathlib module is most efficient for checking if file path exists
-from pathlib import Path
+class TestHomework3(unittest.TestCase):
 
-def create_dataframe(fp):
-	#Check that file path is valid
-	file = Path(fp)
-	if not file.exists():
-		raise ValueError
-	#From https://www.dataquest.io/blog/python-pandas-databases/
-	conn = sqlite3.connect(fp)
-	#Query from HW 1
-	return pd.read_sql_query("SELECT video_id, category_id, 'ca' AS language FROM CAvideos UNION SELECT video_id, category_id, 'de' AS language FROM DEvideos UNION SELECT video_id, category_id, 'fr' AS language FROM FRvideos UNION SELECT video_id, category_id, 'gb' AS language FROM GBvideos UNION SELECT video_id, category_id, 'us' AS language FROM USvideos;", conn)
+	#Test for wrong file path
+	def test_wrong_filepath(self):
+		with self.assertRaises(ValueError):
+			homework3.create_dataframe('x.db')
+
+	#Test for column names
+	def test_column_names(self):
+		df = homework3.create_dataframe('class.db')
+		self.assertTrue(len(df.columns) == 3)
+		self.assertTrue(all(x in df.columns for x in ['video_id', 'category_id', 'language']))
+	
+	#Test for length
+	def test_len(self):
+		df = homework3.create_dataframe('class.db')
+		self.assertTrue(len(df) > 9)
+
+	#Test for key
+	def test_key(self):
+		df = homework3.create_dataframe('class.db')
+		self.assertTrue(len(df.groupby(['video_id', 'language']).count()) == len(df))
+
+
+
+if __name__ == '__main__':
+	unittest.main()
